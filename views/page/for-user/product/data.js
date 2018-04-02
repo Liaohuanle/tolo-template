292bc9849809d48c37e60070dde93f7e70bd375d
tea.liao <tea.liao@bytedance.com>
2018-04-02 23:17:59 +0800

    optimise

--- views/page/for-user/product/data.js
+++ views/page/for-user/product/data.js
@@ -1,3 +1,19 @@
+const breadCrumbList = [
+  {
+    href: '#',
+    title: '全部',
+    isActive: false
+  },{
+    href: '?target=PRC系列',
+    title: 'PRC系列',
+    isActive: false
+  },{
+    href: '?target=RT-PCR',
+    title: 'RT-PCR',
+    isActive: true
+  }
+]
+
 module.exports = {
-  
+  breadCrumbList
 }
