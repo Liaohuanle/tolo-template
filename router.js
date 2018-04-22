1feb0a973c91eff4cd068c63d21a792dfd81bffa
tea.liao <tea.liao@bytedance.com>
2018-04-22 23:21:04 +0800

    add service product

--- router.js
+++ router.js
@@ -1,9 +1,11 @@
 const HOME = require('./views/page/for-user/home/router')
 const PRODUCT = require('./views/page/for-user/product/router')
 const SERVICE = require('./views/page/for-user/service/router')
+const SERVICEHOME = require('./views/page/for-user/service-home/router')
 
 module.exports = [
   HOME,
   PRODUCT,
-  SERVICE
+  SERVICE,
+  SERVICEHOME
 ]
